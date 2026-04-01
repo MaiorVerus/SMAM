cccff722aee6ffabc48a78c10b69ef8f7bcd720e
Major <telvinmaior@gmail.com>
2026-04-01 11:15:15 +0200

    remove RouterProvider and replace with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import LandingPage from './pages/landing-page.jsx';
 import OurServicesPage from './pages/our-services-page.jsx';
-import Decrees from './pages/decrees.jsx';
+// import Decrees from './pages/decrees.jsx';
 
 
 import './css/global-smam.css';
 
 
-const router = createBrowserRouter([
-  
-  { path: "/", element: <Navigate to="/landing" /> },
-  { path: "/landing", element: <LandingPage /> },
-  { path: "/services", element: <OurServicesPage /> },
-  // { path: "/decrees", element: <Decrees /> },
-  
-], { basename: "/SMAM/" });
-
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Routes>
+      <Route path="/" element={<Navigate to="/landing" />} />
+      <Route path="/landing" element={<LandingPage />} />
+      <Route path="/services" element={<OurServicesPage />} />
+      {/* <Route path="/decrees" element={<Decrees />} /> */}
+    </Routes>
   );
 }
 
